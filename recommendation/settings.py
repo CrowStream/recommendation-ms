03b24dc1d4c37278ed8c665d42c5b0e6c75b1e68
Sebastian Rojas <jhsrojasro@unal.edu.co>
2021-12-07 09:05:42 -0500

    allowed hosts

--- recommendation/settings.py
+++ recommendation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-427gk(&gmabj-#diaifnix9(v_6kd8vjt-q8e8akut_a1rdew2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '104.155.130.109']
+ALLOWED_HOSTS = ['*', '0.0.0.0', 'localhost', '104.155.130.109']
 
 
 # Application definition
