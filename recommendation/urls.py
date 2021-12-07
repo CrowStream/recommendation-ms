3ac44bd4c52c0e6ec81c2fde7fd6b9aa9541724c
Sebastian Rojas <jhsrojasro@unal.edu.co>
2021-12-07 09:24:20 -0500

    deleted graphene

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from graphene_django.views import GraphQLView
-from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('recommendation/', include('recommendation_ms.urls')),
-    #path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('admin/', admin.site.urls),
 ]
--- recommendation_ms/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import User, Video
-
-class UserSerializer(serializers.Serializer):
-    user_id = serializers.IntegerField(read_only=True)
-    user_feature
